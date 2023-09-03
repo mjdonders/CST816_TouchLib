4ba8c4813d70930c9f1154a44048011001a80863
mjdonders <33433069+mjdonders@users.noreply.github.com>
2023-09-03 20:57:33 +0200

    Create example_gesture_only.ino

---
+++ examples/example_gesture_only/example_gesture_only.ino
@@ -0,0 +1,45 @@
+#include <arduino.h>
+#include <Wire.h>
+
+#include <CST816_TouchLib.h>
+
+#include "TouchSubscriber.h"
+
+#define PIN_TFT_POWER_ON                 15
+
+using namespace MDO;
+
+TouchSubscriber oTouchSubriber;
+CST816Touch oTouch;
+
+void setup () {
+	Serial.begin(115200);				//so that TouchSubscriber can use that to report
+
+    pinMode(PIN_TFT_POWER_ON, OUTPUT);				//TFT poweron
+    digitalWrite(PIN_TFT_POWER_ON, HIGH);	
+	
+	if (oTouch.init(Wire, &oTouchSubriber)) {
+		Serial.println("Touch screen initialization done");
+	} else {
+		Serial.println("Touch screen initialization failed..");
+	}
+}
+
+void loop () {
+	
+	unsigned long ulStart = millis();
+	while(millis() - ulStart < 20000) {	//Handle gestures for 20 seconds
+		oTouch.control();
+	}
+	
+	//followed by a deep sleep
+	if (oTouch.sleep()) {
+		Serial.println("Touch screen is asleep, now for the controller itself");
+		Serial.println("Touch the screen to wake up");
+		delay(100);
+		esp_deep_sleep_start();	//will wake on touch, this being an ESP32 that will result in a setup() again
+	} else {
+		Serial.println("Initiate a sleep failed");
+	}
+}
+
